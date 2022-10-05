bbec1feeba25f35ccb5713ce00069004abec4716
thiagosalesha <thiagosalesha2@gmail.com>
2022-10-05 16:24:00 -0300

    implementando o delete

--- forum/src/main/java/br/com/salesha/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/salesha/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -72,5 +73,11 @@ public class TopicosController {
 		Topico atualizar = form.atualizar(id, topicoRepository);
 		
 		return ResponseEntity.ok(new TopicoDTO(atualizar));
+	}
+	
+	@DeleteMapping("/{id}") 
+	public ResponseEntity<?> deletar(@PathVariable Long id){
+		topicoRepository.deleteById(id);
+		return ResponseEntity.ok("Fim");
 	}
 }
